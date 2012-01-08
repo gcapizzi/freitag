02eacc44a89931f96ad72d92027899b6a583b0df
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-01-08 20:42:30 +0100

    Documentation.

--- freitag.py
+++ freitag.py
@@ -256,7 +256,7 @@ def extract(mp3s, format, humanize=False):
 
 
 def humanize(mp3s):
-    """Humanize tags in mp3s."""
+    """Humanize album, artist and title tags in mp3s."""
     for mp3 in mp3s:
         _save(mp3, _humanize_tags(tags))
 
