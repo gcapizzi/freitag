48665823f0c6f286121f460f2384666d519ea2d4
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-01-27 01:39:02 +0100

    Huge refactory, added unit tests.

--- freitag.py
+++ freitag.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 #
-# FreiTAG - A simple mp3 command line tool to tag and rename mp3s.
+# FreiTAG - A simple command line tool to tag and rename mp3s.
 # Copyright (c) 2010-2011 Giuseppe Capizzi
 # mailto: g.capizzi@gmail.com
 #
@@ -21,36 +21,44 @@
 
 import argparse
 from sys import exit
-from string import Template
+from string import Template, capwords
+from os import makedirs, sep
+from os.path import dirname, exists
+from shutil import move
+from re import sub, search
 
 from mutagen.mp3 import EasyMP3
 
-TAG_DELIMITER = '%'
-TAG_IDPATTERN = '[a-z]+'
-DEFAULT_FORMAT = "%tracknumber - %artist - %title.mp3"
-TAGS = {
-    'album':       {'abbr': 'b', 'help': 'The album name'},
-    'artist':      {'abbr': 'a', 'help': 'The artist name'},
-    'title':       {'abbr': 't', 'help': 'The track title'},
-    'discnumber':  {'abbr': 'd', 'help': 'The disc number'},
-    'tracknumber': {'abbr': 'n', 'help': 'The track number'},
-    'date':        {'abbr': 'y', 'help': 'The track date (year)'}
- }
 
+class FreiSong:
 
-class FormatTemplate(Template):
-    """Custom `string.Template` subclass used in `_format`."""
-    delimiter = TAG_DELIMITER
-    idpattern = TAG_IDPATTERN
+    """The main FreiTag class, representing a song."""
 
+    TAG_DELIMITER = '%'
+    TAG_IDPATTERN = '[a-z]+'
+    DEFAULT_FORMAT = "%tracknumber - %artist - %title.mp3"
+    TAGS = {
+        'album':       {'abbr': 'b', 'help': 'The album name'},
+        'artist':      {'abbr': 'a', 'help': 'The artist name'},
+        'title':       {'abbr': 't', 'help': 'The track title'},
+        'discnumber':  {'abbr': 'd', 'help': 'The disc number'},
+        'tracknumber': {'abbr': 'n', 'help': 'The track number'},
+        'date':        {'abbr': 'y', 'help': 'The track date (year)'}
+    }
 
-class FreiMP3(EasyMP3):
-    """Custom, simplified `mutagen.mp3.EasyMP3` subclass."""
+    def __init__(self, mp3):
+        self.mp3 = mp3
+        self.prev_filename = mp3.filename
+        self.filename = mp3.filename
+
     def __getitem__(self, key):
         value = ''
 
         try:
-            value = super(EasyMP3, self).__getitem__(key)[0]
+            value = self.mp3[key]
+            # mutagen tags can be arrays
+            if isinstance(value, list):
+                value = value[0]
 
             # remove the slash and everything after it in track number
             # and zero-pad it
@@ -61,204 +69,149 @@ class FreiMP3(EasyMP3):
 
         return value
 
+    def __setitem__(self, key, value):
+        self.mp3[key] = value
+
+    def update(self, tags):
+        """Update song with tags."""
+        # ignore unsupported tags
+        tags = dict((name, value) for name, value in tags.items()
+                if name in self.TAGS and value is not None)
+        # convert everything to unicode
+        tags = dict((name, unicode(value)) for (name, value) in tags.items())
+        self.mp3.update(tags)
+
+    def save(self):
+        """Save the song."""
+        self.mp3.save()
+
+        # rename if necessary
+        if self.filename != self.prev_filename and not exists(self.filename):
+            move(self.prev_filename, self.filename)
+            self.prev_filename = self.filename
+
+    def format(self, format):
+        """Substitute tags in the %tag form with values from dictionary."""
+        class FormatTemplate(Template):
+            delimiter = self.TAG_DELIMITER
+            idpattern = self.TAG_IDPATTERN
+
+        return FormatTemplate(format).safe_substitute(self).strip()
+
+    def rename(self, format):
+        """Rename song according to format."""
+        self.filename = self.format(format)
+
+    def extract(self, format):
+        """Extracts values from a string according to the specified format."""
+        # the regex pattern that matches tags in the format string
+        tag_pattern = '{delimiter}({pattern})'.format(
+                delimiter=self.TAG_DELIMITER, pattern=self.TAG_IDPATTERN)
+
+        def _get_regex_for_tag(m):
+            """Take a match object and return a regex with a properly named
+            group.
+
+            """
+            tag_name = m.group(1)
+            tag_regex = '[^%s]*' % sep
+
+            # non-greedy regex for tracknumber tag
+            if tag_name == 'tracknumber':
+                tag_regex += '?'
+
+            return '(?P<{tag_name}>{tag_regex})'.format(tag_name=tag_name,
+                                                        tag_regex=tag_regex)
+
+        # turn the format string into a regex and parse the filename
+        regex = sub(tag_pattern, _get_regex_for_tag, format)
+
+        self.update(search(regex, self.filename).groupdict())
+
+    def humanize(self):
+        """Humanize album, title and artist tags from tags dictionary."""
+        tags_to_humanize = ['album', 'artist', 'title']
+
+        for tag in tags_to_humanize:
+            if self.mp3.has_key(tag):
+                self.mp3[tag] = capwords(self.mp3[tag].replace('_', ' '))
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('command', choices=['get', 'set', 'rename',
                                             'extract', 'humanize'])
     parser.add_argument('files', nargs='+')
-    parser.add_argument('--format', '-f', default=DEFAULT_FORMAT,
+    parser.add_argument('--format', '-f', default=FreiSong.DEFAULT_FORMAT,
                         help='The format used by "get", "rename" and '
                         + '"extract" commands. You can use the following '
                         + 'placeholders: '
-                        + ', '.join(['%%{0}'.format(t) for t in TAGS]))
+                        + ', '.join(['%%{0}'.format(t) for t in FreiSong.TAGS]))
     parser.add_argument('--humanize', action="store_true", default=False,
                         help='When extracting, convert all fields from '
                         + 'lowecase_with_underscores format to Capitalized '
                         + 'With Spaces format')
 
     # tag setters
-    for tag, props in TAGS.iteritems():
+    for tag, props in FreiSong.TAGS.iteritems():
         long_opt = '--%s' % tag
         short_opt = '-%s' % props['abbr']
         parser.add_argument(long_opt, short_opt, help=props['help'])
 
     args = parser.parse_args()
 
-    mp3s = [FreiMP3(file) for file in args.files]
+    songs = [FreiSong(EasyMP3(file)) for file in args.files]
 
     if args.command == 'get':
-        get(mp3s, args.format)
+        get(songs, args.format)
     elif args.command == 'set':
-        set(mp3s, args)
+        set(songs, args)
     elif args.command == 'rename':
-        rename(mp3s, args.format)
+        rename(songs, args.format)
     elif args.command == 'extract':
-        extract(mp3s, args.format, args.humanize)
+        extract(songs, args.format, args.humanize)
     elif args.command == 'humanize':
-        humanize(mp3s)
+        humanize(songs)
 
 
-def _format(string, dictionary):
-    """Substitute tags in the %tag form with values from dictionary.
-
-    >>> _format("%artist - %title", {'artist': 'Bob Marley', \
-                                     'title':  'One Love'})
-    'Bob Marley - One Love'
-    """
-    return FormatTemplate(string).safe_substitute(dictionary)
-
-
-def get(mp3s, format):
+def get(songs, format):
     """Print the songs informations according to the specified format."""
-    for mp3 in mp3s:
-        print _format(format, mp3).strip()
+    for song in songs:
+        print song.format(format)
 
 
-def _unicode(dictionary):
-    """Convert all values in dictionary to unicode strings.
-
-    >>> _unicode({'artist': 'Bob Marley', 'title': 'One Love'})
-    {'artist': u'Bob Marley', 'title': u'One Love'}
-    """
-    return dict((name, unicode(value)) for (name, value) in dictionary.items())
+def set(songs, args):
+    """Tag songs using arguments from argparse."""
+    for song in songs:
+        song.save(args.__dict__)
 
 
-def _save(mp3, tags):
-    """Update mp3 with tags and save it."""
-    mp3.update(_unicode(tags))
-    mp3.save()
+def rename(songs, format):
+    """Rename songs according to format."""
+    for song in songs:
+        song.rename(format)
 
 
-def _filter_tags(dictionary):
-    """Filter out every entry in dictionary that is not a tag or is None.
-
-    >>> unfiltered = {'artist': 'Bob Marley', 'title': 'One Love', 'foo': 'bar'}
-    >>> filtered = _filter_tags(unfiltered)
-    >>> expected = {'artist': 'Bob Marley', 'title': 'One Love'}
-    >>> filtered == expected
-    True
-    """
-    return dict((name, value) for name, value in dictionary.items()
-                if name in TAGS and value is not None)
-
-
-def set(mp3s, args):
-    """Tag mp3s using arguments from argparse."""
-    for mp3 in mp3s:
-        _save(mp3, _filter_tags(args.__dict__))
-
-
-def rename(mp3s, format):
-    """Rename mp3s according to format."""
-    from os import makedirs
-    from os.path import dirname, exists
-    from shutil import move
-
-    for mp3 in mp3s:
-        dest = _format(format, mp3)
-
-        # create missing directories
-        try:
-            makedirs(dirname(dest))
-        except OSError:
-            pass
-
-        if (exists(dest)):
-            print "%s already exists! Skipping..." % dest
-            return
-        else:
-            move(mp3.filename, dest)
-
-
-def _get_regex_for_tag(m):
-    """Take a match object and return a regex with a properly named group.
-
-    This function is made to be used as replacement function in a `re.sub` call.
-    """
-    from os import sep
-
-    tag_name = m.group(1)
-    tag_regex = '[^%s]*' % sep
-
-    # non-greedy regex for tracknumber tag
-    if tag_name == 'tracknumber':
-        tag_regex += '?'
-
-    return '(?P<{tag_name}>{tag_regex})'.format(tag_name=tag_name,
-                                                tag_regex=tag_regex)
-
-
-def _humanize(string):
-    """Convert string from lowecase_with_underscores to Capitalized With Spaces.
-
-    >>> _humanize('bob_marley_-_one_love')
-    'Bob Marley - One Love'
-    """
-    from string import capwords
-
-    return capwords(string.replace('_', ' '))
-
-
-def _humanize_tags(tags):
-    """Humanize album, title and artist tags from tags dictionary.
-
-    >>> humanized = _humanize_tags({'artist': 'bob marley', \
-                                    'title':  'One_love', \
-                                    'album':  'Exodus'})
-    >>> expected = {'album': 'Exodus', 'title': 'One Love', \
-                    'artist': 'Bob Marley'}
-    >>> humanized == expected
-    True
-    """
-    tags_to_humanize = ['album', 'artist', 'title']
-
-    for tag in tags_to_humanize:
-        if tags.has_key(tag):
-            tags[tag] = _humanize(tags[tag])
-
-    return tags
-
-
-def _extract(string, format):
-    """Extracts values from a string according to the specified format.
-
-    >>> extracted = _extract('Bob Marley - One Love', '%artist - %title')
-    >>> expected = {'artist': 'Bob Marley', 'title': 'One Love'}
-    >>> extracted == expected
-    True
-    """
-    from re import sub, search
-
-    # the regex pattern that matches tags in the format string
-    tag_pattern = '{delimiter}({pattern})'.format(delimiter=TAG_DELIMITER,
-                                                  pattern=TAG_IDPATTERN)
-
-    # turn the format string into a regex and parse the filename
-    regex = sub(tag_pattern, _get_regex_for_tag, format)
-
-    return search(regex, string).groupdict()
-
-
-def extract(mp3s, format, humanize=False):
-    """Tag mp3s extracting tag values from its filename according to format.
+def extract(songs, format, humanize=False):
+    """Tag songs extracting tag values from its filename according to format.
 
     If humanize is True, humanize tags before tagging.
     """
-    for mp3 in mp3s:
-        tags = _extract(mp3.filename, format)
+    for song in songs:
+        song.extract(format)
 
         # humanize
         if humanize:
-            tags = _humanize_tags(tags)
+            song.humanize()
 
-        _save(mp3, tags)
+        song.save()
 
 
-def humanize(mp3s):
-    """Humanize album, artist and title tags in mp3s."""
-    for mp3 in mp3s:
-        _save(mp3, _humanize_tags(mp3))
+def humanize(songs):
+    """Humanize album, artist and title tags in songs."""
+    for song in songs:
+        song.humanize()
+        song.save()
 
 
 if __name__ == '__main__':
---
+++ tests.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# FreiTAG - A simple song command line tool to tag and rename songs.
+# Copyright (c) 2010-2011 Giuseppe Capizzi
+# mailto: g.capizzi@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from freitag import FreiSong
+
+from mutagen.mp3 import EasyMP3
+
+from mock import Mock
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        _tags = {'tracknumber': ['1/2'], 'title': ['One Love'],
+                 'artist': ['Bob Marley'], 'album': ['Exodus']}
+        def getitem(name):
+            return _tags[name]
+        def setitem(name, val):
+            _tags[name] = val
+
+        self.mp3 = Mock()
+        self.mp3.__getitem__ = Mock(side_effect=getitem)
+        self.mp3.__setitem__ = Mock(side_effect=setitem)
+        self.mp3.filename = 'Bob Marley - One Love.mp3'
+
+        self.song = FreiSong(self.mp3)
+
+    def test_getitem(self):
+        self.assertEqual('01',         self.song['tracknumber'])
+        self.assertEqual('One Love',   self.song['title'])
+        self.assertEqual('Bob Marley', self.song['artist'])
+        self.assertEqual('Exodus',     self.song['album'])
+
+    def test_update(self):
+        self.song.update({'artist': 'Dennis Brown', 'title': 'Here I Come',
+                          'foo': 'bar'})
+
+        self.mp3.update.assert_called_with({'artist': 'Dennis Brown',
+                                            'title': 'Here I Come'})
+
+    def test_save(self):
+        self.song.save()
+
+        self.mp3.save.assert_called_with()
+        self.assertEqual(self.song.filename, self.song.prev_filename)
+
+    def test_format(self):
+        format = '%artist - %title'
+
+        self.assertEqual('Bob Marley - One Love', self.song.format(format))
+
+    def test_rename(self):
+        format = '%title - %artist.mp3'
+        self.song.rename(format)
+
+        self.assertEqual('One Love - Bob Marley.mp3', self.song.filename)
+
+    def test_extract(self):
+        format = '%artist - %title.mp3'
+        self.song.filename = 'Dennis Brown - Here I Come.mp3'
+        self.song.extract(format)
+
+        self.mp3.update.assert_called_with({'artist': 'Dennis Brown',
+                                            'title':  'Here I Come'})
+
+    def test_humanize(self):
+        self.song['title']  = 'One_love'
+        self.song['artist'] = 'bob marley'
+        self.song['album']  = 'EXODUS'
+        self.song.humanize()
+
+        self.assertEqual('One Love',   self.song['title'])
+        self.assertEqual('Bob Marley', self.song['artist'])
+        self.assertEqual('Exodus',     self.song['album'])
+
+
+if __name__ == '__main__':
+    unittest.main()
