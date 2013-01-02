214f519a0e3de76942ca1e9d4ef1e9db06760dfc
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-01-02 22:38:44 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='freitag',
-      version='0.1',
-      py_modules=find_packages(),
-      )
+setup(
+    name = 'freitag',
+    version = '0.1',
+    py_modules = find_packages(),
+    scripts = ['bin/freitag'],
+)
