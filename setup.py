1518d410848ca3cb99bb5362094977f66856b33d
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-01-26 21:14:13 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from distutils.core import setup
 
 setup(
     name='freitag',
-    version='0.1.0',
+    version='0.1.1',
     author='G. Capizzi',
     author_email='g.capizzi@gmail.com',
     packages=['freitag'],
     scripts=['bin/freitag'],
-    url='http://pypi.python.org/pypi/freitag/',
-    license='COPYING.txt',
+    url='https://github.com/gcapizzi/freitag',
+    license='GPL 3',
     description='A command-line tool for MP3 tagging and renaming.',
-    long_description=open('README.rst').read(),
     install_requires=[
         "argparse >= 1.2.1",
         "mutagen >= 1.2.0",
