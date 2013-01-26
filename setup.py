676712ccaffe7cd80ced9eec5d07069e9f987b83
Giuseppe Capizzi <g.capizzi@gmail.com>
2013-01-26 18:51:46 +0100

    Complete setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     author_email='g.capizzi@gmail.com',
     packages=['freitag'],
     scripts=['bin/freitag'],
+    url='http://pypi.python.org/pypi/freitag/',
+    license='COPYING.txt',
     description='A command-line tool for MP3 tagging and renaming.',
+    long_description=open('README.rst').read(),
     install_requires=[
         "argparse >= 1.2.1",
         "mutagen >= 1.2.0",
